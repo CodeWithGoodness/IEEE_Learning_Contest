fe38186b5bc89dda5f442210caedb004b398bdc3
Goodness_Adaeze <ceciliaokoro2@gmail.com>
2023-05-26 01:21:16 +0100

    Update faces.py

--- ProblemSet0/MakingFaces/faces.py
+++ ProblemSet0/MakingFaces/faces.py
@@ -12,3 +12,12 @@ def convert(word):
 
 
 main()    
+
+This program has 2 major functions: main() and convert().
+convert():   This function takes in a string parameter and uses the string.replace() function to convert any emoticon[:) and :(] 
+within the string to emoji[🙂 and 🙁]. The converted string is now stored in a new variable called new. The return keyword returns the value of new
+to the convert() function.
+           
+                                                                                                                                
+main(): Prompts the user for input and stores it in the variable, "userInput". It then calls the convert fu
+                                                                                                                           
