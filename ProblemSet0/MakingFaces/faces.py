8bbb18bc04513cf4bd18d854abfb76593d63bedc
Goodness Okoro <ceciliaokoro2@gmail.com>
2023-05-29 10:31:01 +0100

    new commit

--- ProblemSet0/MakingFaces/faces.py
+++ ProblemSet0/MakingFaces/faces.py
@@ -11,16 +11,4 @@ def convert(word):
     return new
 
 
-main()    
-
-This program has 2 major functions: main() and convert().
-convert():   This function takes in a string parameter and uses the string.replace() function to convert any emoticon[:) and :(]
-within the string to emoji[🙂 and 🙁]. The converted string is now stored in a new variable called 'new'. The return keyword returns the value of 'new'
-to the convert() function.
-           
-
-                                                                                                                                
-main(): Prompts the user for input and stores it in the variable, "userInput". It then calls the convert function which checks for emoticons and 
-converts to emojis. The result of this is stired in a vatiable called, "convertedInput" which is then printed.
-  Thus, execution begins from line 14 to line 1-2,  then to line 9-11 and back to line 4                                                                                                   
-                                                                                                                           
+main()                                                                                                      
---
+++ ProblemSet2/camelCase/camelCase.py
@@ -0,0 +1,26 @@
+def main():
+    #prompt the user for input
+    in_camel_case = input("CamelCase:")
+    to_snake_case(in_camel_case)
+
+def to_snake_case(word):
+    for i in range(len(word)):
+        if word[i].isupper():
+            print("_",(word[i]).lower(), end="")
+        else:
+            print(word[i], end="")
+    i + 1
+    
+main()
+
+    #if split_words.isUpper
+
+    
+
+
+
+
+
+
+
+
